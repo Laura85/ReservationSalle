e05a748ed69f4e8a09f0be27d25b73873a1891c9
Zineb ADIB <zineb.elbekkali@etu.univ-tours.fr>
2016-01-16 12:38:12 +0100

    tests unitaires pour la gestion des salles

--- src/test/junit/ClasseTestSalle.java
+++ src/test/junit/ClasseTestSalle.java
@@ -1,21 +1,46 @@
 package test.junit;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import junit.framework.Assert;
 import junit.framework.TestCase;
+import reservationsalle.BDD;
 
 public class ClasseTestSalle extends TestCase {
 
-	public void testInsertSalle() {
-		// TODO Auto-generated method stub
+	public void testInsertSalle() throws SQLException {
+		
+		BDD bdd= new BDD();
+		bdd.insertSalle("WindowsA");
+		String sql="SELECT nomSalle FROM salles;";
+		ResultSet rs=bdd.st.executeQuery(sql);
+		Assert.assertEquals("WindowsA", rs.next());
+		bdd.suppSalle("WindowsA");
 
 	}
 	
-	public void testUpdateSalle() {
-		// TODO Auto-generated method stub
+	public void testUpdateSalle() throws SQLException {
+		
+		BDD bdd= new BDD();
+		bdd.insertSalle("WindoB");
+		bdd.UpdateSalle("WindowsB", "WindoB");
+		String sql="SELECT nomSalle FROM salles WHERE nomSalle = WindowsB;";
+		ResultSet rs=bdd.st.executeQuery(sql);
+		Assert.assertEquals(true, rs.next());
+		bdd.suppSalle("WindowsB");
+
 
 	}
 	
-	public void testDeleteSalle() {
-		// TODO Auto-generated method stub
+	public void testDeleteSalle() throws SQLException {
+		
+		BDD bdd= new BDD();
+		bdd.insertSalle("Pascal");
+		bdd.suppSalle("Pascal");
+		String sql="SELECT nomSalle FROM salles WHERE nomSalle = Pascal;";
+		ResultSet rs=bdd.st.executeQuery(sql);
+		Assert.assertEquals(false, rs.next());
 
 	}
 
