c54668342e013de4e848b929c6aa02de1f3c3454
lauraPolytech <laura.bellego@etu.univ-tours.fr>
2016-01-16 11:25:12 +0100

    test junitsalle

---
+++ src/test/junit/ClasseTestSalle.java
@@ -0,0 +1,22 @@
+package test.junit;
+
+import junit.framework.TestCase;
+
+public class ClasseTestSalle extends TestCase {
+
+	public void testInsertSalle() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void testUpdateSalle() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void testDeleteSalle() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
