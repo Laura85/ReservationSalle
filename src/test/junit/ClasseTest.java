61aacfe6fddbaa587dcfe28ccee92bdb210dff34
lauraPolytech <laura.bellego@etu.univ-tours.fr>
2016-01-16 10:49:22 +0100

    JUnit

---
+++ src/test/junit/ClasseTest.java
@@ -0,0 +1,12 @@
+package test.junit;
+
+import junit.framework.TestCase;
+
+public class ClasseTest extends TestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
