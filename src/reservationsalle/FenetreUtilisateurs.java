806c54b3baef8f81e8c597df6e9afabc9140dbb3
Annabelle <Annabelle@pc-anna>
2016-01-16 10:18:47 +0100

    Anna gestionUti

--- src/reservationsalle/FenetreUtilisateurs.java
+++ src/reservationsalle/FenetreUtilisateurs.java
@@ -21,6 +21,7 @@ import javax.swing.JLayeredPane;
 import javax.swing.BoxLayout;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 
@@ -62,75 +63,83 @@ public class FenetreUtilisateurs extends JFrame {
 
 		DefaultTableModel modele = remplirTable();
 		contentPane.setLayout(null);
-		
+
 		JButton btnAjouter = new JButton("Ajouter");
 		btnAjouter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(arg0.getSource() == btnAjouter)
-			      {
+				{
 					FenetreAjoutUti fen2 = new FenetreAjoutUti();
 					fen2.setModal(true);
 					fen2.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			           fen2.setVisible(true);
-			           DefaultTableModel modele2 = remplirTable();
-			           table.setModel(modele2);
+					fen2.setVisible(true);
+					DefaultTableModel modele2 = remplirTable();
+					table.setModel(modele2);
 
-			       }
+				}
 			}
 		});
 		btnAjouter.setBounds(24, 238, 89, 23);
 		contentPane.add(btnAjouter);
 		scrollPane.setBounds(37, 37, 335, 126);
 		contentPane.add(scrollPane);
-		
-				table = new JTable(modele);
-				scrollPane.setViewportView(table);
-				
-				JButton btnSupprimer = new JButton("Supprimer");
-				btnSupprimer.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						int ligneSelectionne = table.getSelectedRow();
-						//on r�cup�re la valeur de la premi�re colonne de la ligne s�lectionn�
-						table.getValueAt(ligneSelectionne, 0);
-						System.out.println(table.getValueAt(ligneSelectionne, 0));
-						String suppUti = "DELETE FROM utilisateurs WHERE idUtilisateur ='"+table.getValueAt(ligneSelectionne, 0)+"'";
-						Connection con1 = BDD_General.connect();
-						Statement stmt;
-						try {
-							stmt = con1.createStatement();
-							int up = stmt.executeUpdate(suppUti);
-							DefaultTableModel modele2 = remplirTable();
-					        table.setModel(modele2);
-						} catch (SQLException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
+
+		table = new JTable(modele);
+		scrollPane.setViewportView(table);
+
+		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(table.getSelectedRow()==-1){
+					JOptionPane.showMessageDialog(null, "Veuillez selectionner une ligne.");
+				}else{
+					int ligneSelectionne = table.getSelectedRow();
+					//on r�cup�re la valeur de la premi�re colonne de la ligne s�lectionn�
+					table.getValueAt(ligneSelectionne, 0);
+					System.out.println(table.getValueAt(ligneSelectionne, 0));
+					String suppUti = "DELETE FROM utilisateurs WHERE idUtilisateur ='"+table.getValueAt(ligneSelectionne, 0)+"'";
+					Connection con1 = BDD_General.connect();
+					Statement stmt;
+					try {
+						stmt = con1.createStatement();
+						int up = stmt.executeUpdate(suppUti);
+						DefaultTableModel modele2 = remplirTable();
+						table.setModel(modele2);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
-				});
-				btnSupprimer.setBounds(123, 238, 89, 23);
-				contentPane.add(btnSupprimer);
-				
-				btnModifier = new JButton("Modifier");
-				btnModifier.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						// r�cup�ration de la ligne selectionn�e
-						int ligneSelectionne = table.getSelectedRow();
-						//on r�cup�re la valeur de la premi�re colonne de la ligne s�lectionn�
-						Object o1 = table.getValueAt(ligneSelectionne, 0);
-						System.out.println(table.getValueAt(ligneSelectionne, 0));
-						
-						FenetreModifUti fen3 = new FenetreModifUti(o1);
-						fen3.setModal(true);
-						fen3.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-						fen3.setVisible(true);
-				           DefaultTableModel modele3 = remplirTable();
-				           table.setModel(modele3);
-					}
-				});
-				btnModifier.setBounds(222, 238, 89, 23);
-				contentPane.add(btnModifier);
-		
-	
+				}
+			}
+		});
+		btnSupprimer.setBounds(123, 238, 89, 23);
+		contentPane.add(btnSupprimer);
+
+		btnModifier = new JButton("Modifier");
+		btnModifier.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(table.getSelectedRow()==-1){
+					JOptionPane.showMessageDialog(null, "Veuillez selectionner une ligne.");
+				}else{
+				// r�cup�ration de la ligne selectionn�e
+				int ligneSelectionne = table.getSelectedRow();
+				//on r�cup�re la valeur de la premi�re colonne de la ligne s�lectionn�
+				Object o1 = table.getValueAt(ligneSelectionne, 0);
+				System.out.println(table.getValueAt(ligneSelectionne, 0));
+
+				FenetreModifUti fen3 = new FenetreModifUti(o1);
+				fen3.setModal(true);
+				fen3.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				fen3.setVisible(true);
+				DefaultTableModel modele3 = remplirTable();
+				table.setModel(modele3);
+			}
+			}
+		});
+		btnModifier.setBounds(222, 238, 89, 23);
+		contentPane.add(btnModifier);
+
+
 	}
 
 	static DefaultTableModel remplirTable(){
@@ -143,16 +152,16 @@ public class FenetreUtilisateurs extends JFrame {
 		DefaultTableModel modele;
 		modele = new DefaultTableModel();
 		int nbUti=0;
-		
+
 		try {
 			Connection con1 = BDD_General.connect();
 			Statement stmt;
-		
+
 			stmt = con1.createStatement();
 			ResultSet resultat1 = null;
 			resultat1 = stmt.executeQuery(requete1);
 			resultat1.next();
-			
+
 			nbUti = resultat1.getInt("compt");
 			String [][] conto = new String[nbUti][2];
 			Connection con = BDD_General.connect();
@@ -176,14 +185,14 @@ public class FenetreUtilisateurs extends JFrame {
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-			
+
 		}
 		return modele;
-		
 
 
-	
-		
+
+
+
 
 
 	}	
